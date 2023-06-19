ffd0c316510ec6b6aea3c5a93f60bcd59e536316
AtmegaBuzz <swapnilshinde9382@gmail.com>
2023-06-19 13:10:35 +0530

    added merge sort

---
+++ step2/sorting2/merge_sort.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <limits.h>
+using namespace std;
+
+void merge(int arr[],int start, int mid, int end){
+
+
+    int l_size = mid-start+1;
+    int left[l_size + 1];
+
+    int r_size = end-(mid+1) + 1;
+    int right[r_size + 1];
+
+    for(int i=0;i<l_size;i++){
+        left[i] = arr[start+i];
+    }
+
+    for(int i=0;i<r_size;i++){
+        right[i] = arr[mid+1+i];
+    }
+
+    left[l_size] = right[r_size] = INT_MAX;
+    int l_i = 0;
+    int r_i = 0;
+
+    for(int i=start;i<=end;i++){
+        if(left[l_i] < right[r_i]){
+            arr[i] = left[l_i];
+            l_i++;
+        }
+        else{
+            arr[i] = right[r_i];
+            r_i++;
+        }
+    }
+
+
+
+}
+
+void mergeSort(int arr[],int start,int end){
+
+    // cout << start << " " << end << endl;
+
+    if(start==end) return;
+
+    int mid = (start+end)/2;
+    mergeSort(arr,start,mid);
+    mergeSort(arr,mid+1,end);
+    merge(arr,start,mid,end);    
+
+}
+
+
+int main(){
+
+    int arr[] = {13,46,24,52,20,9};
+    mergeSort(arr,0,5);
+
+    for(int i=0;i<6;i++){
+        cout << arr[i] << " ";
+    }
+
+
+    return 0;
+}
